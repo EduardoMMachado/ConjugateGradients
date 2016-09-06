1f74feb2861a21c6825ee4b0afdd92fa1b51fcf1
BRUNA DE FREITAS MACIEL <bfm13@inf.ufpr.br>
2016-09-06 14:49:31 -0300

    inserção do algoritmo do livro, para futura analise

--- src/cgMethod.c
+++ src/cgMethod.c
@@ -19,14 +19,75 @@ int conjugateGradients(double **A, double *b, double *x, unsigned int N, unsigne
 	double stepSize;
 
 	// Aloca vetores de resíduo e norma
-	double *r (double*) malloc(N*sizeof(double));
-	double *norm (double*) malloc(maxIter*sizeof(double));
-	double *error (double*) malloc(maxIter*sizeof(double));
+	double *r = (double*) malloc(N*sizeof(double));
+	double *norm = (double*) malloc(maxIter*sizeof(double));
+	double *error = (double*) malloc(maxIter*sizeof(double));
 
 	// Inicializa os resíduos e norma para a iteração 0
+	
 	rTimeStart = timestamp();
 	residueCalculator(r, A, x, b, N);
 	rTimeEnd = timestamp();
+
+//============================= ALGORITMO DO LIVRO ===========================================
+	double *v = (double*) malloc(N*sizeof(double)); //vetor de direçao
+	double *s = (double*) malloc(N*sizeof(double)); //vetor de passos
+	double *z = (double*) malloc(N*sizeof(double));	
+	double *aux = (double*) malloc(N*sizeof(double));
+	int i,j;
+
+	//r=b e v=b
+	for (i = 0; i < N; i++)
+    	v[i] = r[i] = b[i];
+
+    //aux = rt*r
+    //na teoria rt=r pois r eh vetor entao, aux = r*r
+    for (i = 0; i < N; i++)
+    	aux[i] = r[i] *r[i];
+
+    norm[n_iter] = normCalculator(r, N);
+	error[n_iter] = norm[n_iter];
+
+    while(n_iter <= maxIter){
+	
+		//z=Av
+		for (i = 0; i < N; i++) {
+        	for (j = 0; j < N; j++)
+          		z[i] += A[i*N+j] * v[j];
+      	}
+
+    	//s = (rt*r)/vt*z = aux /vt*z
+
+    	//s_aux = vt*z
+    	for (i = 0; i < N; i++){
+    		s = aux[i] / v[i] * z[i];
+    	}
+
+    	//prox x = x + s*v
+    	for (i = 0; i < N; i++) {
+    		x[i] = x[i] + s[i]*v[i];
+    	}
+
+    	//calcula o residuo -> r= r - s*z
+    	rTimeStart = timestamp();
+		residueCalculator(r, s, z);
+		rTimeEnd = timestamp();
+	  	
+	  	norm[n_iter] = normCalculator(r, N);
+		error[n_iter] = abs(norm[n_iter] - norm[n_iter-1]);
+		
+		//se o erro for menor que a tolerancia retorna 0 e a funçao acaba com sucesso, e o vetor x eh a soluçao
+		if(error[n_iter] < tolerance)
+			return 0;
+
+		//calcula novo vetor de direçoes
+		for (i = 0; i < N; i++)
+    		v[i] = r[i] + ( norm[n_iter]/norm[n_iter-1] ) *v[i];
+
+
+    }
+
+ //=============================== FIM METODO DO LIVRO ==============================================
 
 	norm[n_iter] = normCalculator(r, N);
 	error[n_iter] = norm[n_iter];
