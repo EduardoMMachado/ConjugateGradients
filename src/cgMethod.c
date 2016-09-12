ffa619314dc94a578148ce83889930ea230c857f
Eduardo Machado <emm14@inf.ufpr.br>
2016-09-12 14:40:25 -0300

    arrumado algumas coisinhas.

--- src/cgMethod.c
+++ src/cgMethod.c
@@ -24,7 +24,7 @@ int conjugateGradients(double **A, double *b, double *x, unsigned int N, unsigne
 	double *error = (double*) malloc(maxIter*sizeof(double));
 
 	// Inicializa os resíduos e norma para a iteração 0
-	
+
 	rTimeStart = timestamp();
 	residueCalculator(r, A, x, b, N);
 	rTimeEnd = timestamp();
@@ -32,50 +32,50 @@ int conjugateGradients(double **A, double *b, double *x, unsigned int N, unsigne
 //============================= ALGORITMO DO LIVRO ===========================================
 	double *v = (double*) malloc(N*sizeof(double)); //vetor de direçao
 	double *s = (double*) malloc(N*sizeof(double)); //vetor de passos
-	double *z = (double*) malloc(N*sizeof(double));	
+	double *z = (double*) malloc(N*sizeof(double));
 	double *aux = (double*) malloc(N*sizeof(double));
 	int i,j;
 
 	//r=b e v=b
 	for (i = 0; i < N; i++)
-    	v[i] = r[i] = b[i];
+    v[i] = r[i] = b[i];
 
-    norm[n_iter] = normCalculator(r, N);
-	
-    for(n_iter = 0 ; n_iter <= maxIter ; n_iter++){ // COLOCAR SAIDA COM ERRO POR CAUSA DA IMPRESSAO NO ARQ
-    	
-		//z=Av
-		for (i = 0; i < N; i++) {
-        	for (j = 0; j < N; j++)
-          		z[i] += A[i*N+j] * v[j]; //ALTERAR INDICES DE A -> SO TEMOS DIAGONAIS
-      	}
+  norm[n_iter] = normCalculator(r, N);
 
-    	//s = NORMA/vt*z
-    	for (i = 0; i < N; i++){
-    		s = norm[n_iter] / v[i] * z[i];
-    	}
+  for(n_iter = 0 ; n_iter <= maxIter ; n_iter++){ // COLOCAR SAIDA COM ERRO POR CAUSA DA IMPRESSAO NO ARQ
+
+  	//z=Av
+  	for (i = 0; i < N; i++) {
+    	for (j = 0; j < N; j++)
+      		z[i] += A[i*N+j] * v[j]; //ALTERAR INDICES DE A -> SO TEMOS DIAGONAIS
+  	}
+
+  	//s = NORMA/vt*z
+  	for (i = 0; i < N; i++){
+  		s = norm[n_iter] / v[i] * z[i];
+  	}
 
 
-    	//prox x = x + s*v
-    	for (i = 0; i < N; i++) {
-    		x[i] = x[i] + s[i]*v[i];
-    	}
+  	//prox x = x + s*v
+  	for (i = 0; i < N; i++) {
+  		x[i] = x[i] + s[i]*v[i];
+  	}
 
-    	//calcula o residuo -> r= r - s*z
-    	rTimeStart = timestamp();
+  	//calcula o residuo -> r= r - s*z
+  	rTimeStart = timestamp();
 		residueCalculator(r, s, z);
 		rTimeEnd = timestamp();
-	  	
-	  	norm[n_iter] = normCalculator(r, N);
+
+  	norm[n_iter] = normCalculator(r, N);
 		error[n_iter] = abs(norm[n_iter] - norm[n_iter-1]);
-		
+
 		//se o erro for menor que a tolerancia retorna 0 e a funçao acaba com sucesso, e o vetor x eh a soluçao
 		if(error[n_iter] < tolerance)
 			return 0;
 
 		//calcula novo vetor de direçoes
 		for (i = 0; i < N; i++)
-    		v[i] = r[i] + ( norm[n_iter]/norm[n_iter-1] ) *v[i];
+  		v[i] = r[i] + ( norm[n_iter]/norm[n_iter-1] ) *v[i];
 
 
     }
